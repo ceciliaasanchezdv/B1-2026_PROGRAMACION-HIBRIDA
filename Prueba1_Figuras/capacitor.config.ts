28d3175f7ca5c20037d97efaf739d2512362f2b9
ceciliaasanchezdv <ceciliaasanchezdv@gmail.com>
2026-04-12 23:46:09 -0400

    Add files via upload

---
+++ Prueba1_Figuras/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.prueba1figuras',
+  appName: 'prueba1figuras',
+  webDir: 'www'
+};
+
+export default config;
