aa48c78e078197e6fc43186c50469d50c446e75f
ceciliaasanchezdv <ceciliaasanchezdv@gmail.com>
2026-04-27 22:17:06 -0400

    Delete src directory

--- src/app/models/cita.modelo.ts
+++
@@ -1,6 +0,0 @@
-//Define la estructura de una cita en la app
-export interface Cita {
-  id: number;       //Identificador único de cada cita
-  frase: string;    //Texto de la cita
-  autor: string;    //Autor de la cita
-}
