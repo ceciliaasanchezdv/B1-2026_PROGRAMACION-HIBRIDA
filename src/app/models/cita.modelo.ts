dced54042a0db1d9b9b0db85bbc7d9b43cc60817
ceciliaasanchezdv <ceciliaasanchezdv@gmail.com>
2026-04-27 22:16:20 -0400

    Add files via upload

---
+++ src/app/models/cita.modelo.ts
@@ -0,0 +1,6 @@
+//Define la estructura de una cita en la app
+export interface Cita {
+  id: number;       //Identificador único de cada cita
+  frase: string;    //Texto de la cita
+  autor: string;    //Autor de la cita
+}
