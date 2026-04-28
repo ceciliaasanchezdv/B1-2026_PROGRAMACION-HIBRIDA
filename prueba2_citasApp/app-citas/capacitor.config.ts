6ef1ec644433191005013617d86b5026a4555d71
ceciliaasanchezdv <ceciliaasanchezdv@gmail.com>
2026-04-27 20:42:18 -0400

    Delete prueba2_citasApp/app-citas directory

--- prueba2_citasApp/app-citas/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app-citas',
-  webDir: 'www'
-};
-
-export default config;
