1183957ac9a53e83e45a5eab115fe5146de15ff7
ceciliaasanchezdv <ceciliaasanchezdv@gmail.com>
2026-05-18 06:45:28 -0400

    Add files via upload

---
+++ PruebaFinal_peor-es-nada/peor-es-nada/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'peor.es.nada',
+  appName: 'peor-es-nada',
+  webDir: 'www'
+};
+
+export default config;
